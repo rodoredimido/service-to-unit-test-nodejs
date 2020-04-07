3f6e6b97ce7f3406f7ae36e3f3ce5b3440c86945
Diego <oliveiraj.diego@gmail.com>
2020-04-07 10:58:59 -0300

    File logger.js changed

--- src/libs/logger.js
+++ src/libs/logger.js
@@ -1,10 +1,19 @@
 const winston = require('winston')
 const config = require('../../configs')
 
-const console = new winston.transports.Console({ level: 'info' })
-const file = new winston.transports.File({ filename: 'combined.log' })
+const options = {
+  file: {
+    filename: 'server.log',
+    maxsize: 5000000,
+    maxFiles: 3
+  },
+  console: {
+    level: 'info'
+  }
+}
 
-const logger = winston.createLogger({
+// eslint-disable-next-line new-cap
+const logger = new winston.createLogger({
   level: 'info',
   format: winston.format.combine(
     winston.format.timestamp({
@@ -15,13 +24,13 @@ const logger = winston.createLogger({
   ),
   defaultMeta: { service: 'node_app_test' },
   transports: [
-    console,
-    file
+    new winston.transports.File(options.file)
   ]
 })
 
-if (config.node_env !== 'production') {
-  logger.remove(console)
+if (config.node_env === 'production') {
+  logger.add(new winston.transports.Console(options.console))
+} else {
   logger.add(new winston.transports.Console({
     format: winston.format.simple()
   }))
